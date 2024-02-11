41ae5d4fd716bfa8fd123f84eb583dfc6da811a2
Biokes <106444765+Biokes@users.noreply.github.com>
2024-02-11 14:46:29 +0100

    my wowkspace
    Signed-off-by: Biokes <106444765+Biokes@users.noreply.github.com>

--- src/assignmentsAndTasks/MyStack.java
+++ src/assignmentsAndTasks/MyStack.java
@@ -1,29 +1,39 @@
 package assignmentsAndTasks;
 
 public class MyStack {
-    private String [] stack = new String [0];
-    private  int size=0;
-    public void createStack(int number) {
+    private final String [] stack ;
+    private int nextVacant =0;
+    public MyStack(int number) {
         if(number < 1)
-            throw new IllegalArgumentException("Stack of a negative length is not valid");
+            throw new IllegalArgumentException(String.format("%s length is not valid for creating a stack.",number));
         stack = new String[number];
     }
 
     public boolean isEmpty() {
-        return true;
+        if(nextVacant == 0)
+            return true;
+        return false;
     }
 
     public void push(String name) {
-
+        if(nextVacant+1 >stack.length )
+            throw new StackOverflowError("Elements cannot be pushed beyond Stack size");
+        stack[nextVacant++] = name;
     }
 
     public String peek() {
-        String output = "";
+        String output;
         for(int counter = stack.length-1; counter >= 0; counter-- ){
             if(stack[counter] == null)
                 continue;
-                output = stack[counter];
-                return output;
-        }        return "Stack is empty";
+            output = stack[counter];
+            return output;
+        }
+        return "Stack is empty";
+    }
+
+    public void pop(String name) {
+        if(nextVacant ==0 )
+            throw new RuntimeException("Stack UnderFlow.");
     }
 }
